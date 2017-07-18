da93f9214e5c85592dc1a2b1d4085c73f39bbc1a
Mirrgie Riana <tacticsrealize2@gmail.com>
2017-07-19 04:08:58 +0900

    sync: update

--- titanium.solar.synchronizer/src/main/java/titanium/solar/synchronizer/Main.java
+++ titanium.solar.synchronizer/src/main/java/titanium/solar/synchronizer/Main.java
@@ -2,6 +2,7 @@ package titanium.solar.synchronizer;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.stream.Stream;
 
 import com.pi4j.io.gpio.GpioController;
 import com.pi4j.io.gpio.GpioFactory;
@@ -17,24 +18,102 @@ import mirrg.lithium.properties.PropertiesMultipleInheritable;
 public class Main
 {
 
+	private static int highUs;
+	private static int zeroLowUs;
+	private static int oneLowUs;
+
 	public static void main(String[] args) throws Exception
 	{
 		IProperties properties = parse("default.synchronizer.properties", args);
 
 		int periodMs = properties.get("periodMs").getInteger().get();
-		int durationMs = properties.get("durationMs").getInteger().get();
 		String gpioPin = properties.get("gpioPin").getString().get();
+		highUs = properties.get("highUs").getInteger().get();
+		zeroLowUs = properties.get("zeroLowUs").getInteger().get();
+		oneLowUs = properties.get("oneLowUs").getInteger().get();
+		int[] data = Stream.of(properties.get("data").getString().get().split("\\s+"))
+			.mapToInt(s -> Integer.parseInt(s, 10))
+			.toArray();
 
 		GpioController gpio = GpioFactory.getInstance();
-		Pin gpio01 = (Pin) RaspiPin.class.getField(gpioPin).get(null);
-		GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(gpio01, "titanium.solar.synchronizer", PinState.LOW);
-
+		Pin pinName = (Pin) RaspiPin.class.getField(gpioPin).get(null);
+		GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(pinName, Main.class.getPackage().getName(), PinState.LOW);
 		while (true) {
-			pin.pulse(durationMs, false);
+			new Thread(() -> {
+				try {
+					sendMasterPacket(pin, data);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				return;
+			}).start();
 			Thread.sleep(periodMs);
 		}
 
 		//gpio.shutdown();
+	}
+
+	private static void sendMasterPacket(GpioPinDigitalOutput pin, int[] data) throws InterruptedException
+	{
+		sendPre(pin);
+		for (int i : data) {
+			sendByte(pin, i);
+		}
+		sendPost(pin);
+	}
+
+	private static void sendByte(GpioPinDigitalOutput pin, int b) throws InterruptedException
+	{
+		for (int i = 0; i < 8; i++) {
+			if ((b & (1 << i)) != 0) {
+				sendOne(pin);
+			} else {
+				sendZero(pin);
+			}
+		}
+	}
+
+	private static void sendPre(GpioPinDigitalOutput pin) throws InterruptedException
+	{
+		sendOne(pin);
+		sendOne(pin);
+		sendOne(pin);
+		sendOne(pin);
+	}
+
+	private static void sendPost(GpioPinDigitalOutput pin) throws InterruptedException
+	{
+		sendOne(pin);
+		sendBlank(pin);
+	}
+
+	private static void sendZero(GpioPinDigitalOutput pin) throws InterruptedException
+	{
+		pin.high();
+		sleep(highUs);
+		pin.low();
+		sleep(zeroLowUs);
+	}
+
+	private static void sendOne(GpioPinDigitalOutput pin) throws InterruptedException
+	{
+		pin.high();
+		sleep(highUs);
+		pin.low();
+		sleep(oneLowUs);
+	}
+
+	private static void sendBlank(GpioPinDigitalOutput pin) throws InterruptedException
+	{
+		sleep(5000);
+	}
+
+	private static void sleep(int micros)
+	{
+		long start = System.nanoTime();
+		while (System.nanoTime() < start + micros * 1000) {
+
+		}
 	}
 
 	private static IProperties parse(String defaultPropertyFileName, String... args) throws Exception
