6d07a897625afc2f225de3404ea901d592993f4a
Mirrgie Riana <tacticsrealize2@gmail.com>
2017-09-28 22:02:44 +0900

    kisyou: 0.1.0

--- titanium.solar.libs.kisyou/src/main/java/titanium/solar/libs/kisyou/HKisyou.java
+++ titanium.solar.libs.kisyou/src/main/java/titanium/solar/libs/kisyou/HKisyou.java
@@ -5,6 +5,8 @@ import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Optional;
+import java.util.OptionalDouble;
 import java.util.OptionalInt;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -69,15 +71,29 @@ public class HKisyou
 		while (matcher.find()) {
 			int hour = Integer.parseInt(matcher.group(1), 10);
 			int minute = Integer.parseInt(matcher.group(2), 10);
-			double kousui = Double.parseDouble(matcher.group(3));
-			double temperature = Double.parseDouble(matcher.group(4));
-			double averageHuusoku = Double.parseDouble(matcher.group(5));
-			String averageKazamuki = matcher.group(6);
-			double maxHusoku = Double.parseDouble(matcher.group(7));
-			String maxKazamuki = matcher.group(8);
+			OptionalDouble kousui = matcher.group(3).equals("///")
+				? OptionalDouble.empty()
+				: OptionalDouble.of(Double.parseDouble(matcher.group(3)));
+			OptionalDouble temperature = matcher.group(4).equals("///")
+				? OptionalDouble.empty()
+				: OptionalDouble.of(Double.parseDouble(matcher.group(4)));
+			OptionalDouble averageHuusoku = matcher.group(5).equals("///")
+				? OptionalDouble.empty()
+				: OptionalDouble.of(Double.parseDouble(matcher.group(5)));
+			Optional<String> averageKazamuki = matcher.group(6).equals("///")
+				? Optional.empty()
+				: Optional.of(matcher.group(6));
+			OptionalDouble maxHusoku = matcher.group(7).equals("///")
+				? OptionalDouble.empty()
+				: OptionalDouble.of(Double.parseDouble(matcher.group(7)));
+			Optional<String> maxKazamuki = matcher.group(8).equals("///")
+				? Optional.empty()
+				: Optional.of(matcher.group(8));
 			OptionalInt nissyou = matcher.group(9).isEmpty()
 				? OptionalInt.empty()
-				: OptionalInt.of(Integer.parseInt(matcher.group(9), 10));
+				: matcher.group(9).equals("///")
+					? OptionalInt.empty()
+					: OptionalInt.of(Integer.parseInt(matcher.group(9), 10));
 
 			kisyouEntries.add(new KisyouEntry(
 				hour,
--- titanium.solar.libs.kisyou/src/main/java/titanium/solar/libs/kisyou/KisyouEntry.java
+++ titanium.solar.libs.kisyou/src/main/java/titanium/solar/libs/kisyou/KisyouEntry.java
@@ -1,6 +1,8 @@
 package titanium.solar.libs.kisyou;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.OptionalDouble;
 import java.util.OptionalInt;
 
 public class KisyouEntry
@@ -9,24 +11,24 @@ public class KisyouEntry
 	public final int hour;
 	public final int minute;
 	public final LocalDateTime time;
-	public final double kousui;
-	public final double temperature;
-	public final double averageHuusoku;
-	public final String averageKazamuki;
-	public final double maxHusoku;
-	public final String maxKazamuki;
+	public final OptionalDouble kousui;
+	public final OptionalDouble temperature;
+	public final OptionalDouble averageHuusoku;
+	public final Optional<String> averageKazamuki;
+	public final OptionalDouble maxHusoku;
+	public final Optional<String> maxKazamuki;
 	public final OptionalInt nissyou;
 
 	public KisyouEntry(
 		int hour,
 		int minute,
 		LocalDateTime time,
-		double kousui,
-		double temperature,
-		double averageHuusoku,
-		String averageKazamuki,
-		double maxHusoku,
-		String maxKazamuki,
+		OptionalDouble kousui,
+		OptionalDouble temperature,
+		OptionalDouble averageHuusoku,
+		Optional<String> averageKazamuki,
+		OptionalDouble maxHusoku,
+		Optional<String> maxKazamuki,
 		OptionalInt nissyou)
 	{
 		this.hour = hour;
@@ -46,9 +48,9 @@ public class KisyouEntry
 	{
 		return String.format("%s,%s,%s,%s",
 			time,
-			kousui,
-			temperature,
-			nissyou.orElse(-1));
+			kousui.isPresent() ? kousui : "null",
+			temperature.isPresent() ? temperature : "null",
+			nissyou.isPresent() ? nissyou : "null");
 	}
 
 }
